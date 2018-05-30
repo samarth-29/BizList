11f91a6c79aada398162802adf03f3508422521f
samarth-29 <32147591+samarth-29@users.noreply.github.com>
2018-05-30 11:24:34 -0700

    Update businesses.js

--- routes/businesses.js
+++ routes/businesses.js
@@ -30,11 +30,23 @@ router.get('/add', function(req, res, next) {
 });
 
 router.get('/show/:id', function(req, res, next) {
-  res.send('respond with a resource');
+  couch.get("bizlist", req.params.id).then(({data, headers, status}) => {
+    res.render('show', {
+      business: data
+    });
+  }, err => {
+      res.send(err);
+  });
 });
 
 router.get('/edit/:id', function(req, res, next) {
-  res.send('respond with a resource');
+  couch.get("bizlist", req.params.id).then(({data, headers, status}) => {
+    res.render('editbusiness', {
+      business: data
+    });
+  }, err => {
+      res.send(err);
+  });
 });
 
 router.get('/category/:category', function(req, res, next) {
@@ -74,11 +86,57 @@ router.post('/add', function(req, res, next) {
 });
 
 router.post('/edit/:id', function(req, res, next) {
-  res.send('respond with a resource');
+  req.checkBody('name', 'Name is required').notEmpty();
+  req.checkBody('category', 'Category is required').notEmpty();
+  req.checkBody('city', 'City is required').notEmpty();
+
+  var errors = req.validationErrors();
+
+  if(errors){
+    couch.get("bizlist", req.params.id).then(({data, headers, status}) => {
+      res.render('editbusiness', {
+        business: data,
+        errors: errors
+      });
+    }, err => {
+        res.send(err);
+    });
+      } else {
+        couch.get("bizlist", req.params.id).then(({data, headers, status}) => {
+          couch.update("bizlist", {
+          _id: req.params.id,
+          _rev: data._rev,
+          name: req.body.name,
+          category: req.body.category,
+          website: req.body.website,
+          phone: req.body.phone,
+          address: req.body.address,
+          city: req.body.city,
+          state: req.body.state,
+          zip: req.body.zip
+      }).then(({data, headers, status}) => {
+        req.flash('success', 'Business Updated');
+        res.redirect('/businesses');
+      }, err => {
+        res.send(err);
+      });
+        }, err => {
+            res.send(err);
+        });
+  }
 });
 
 router.post('/delete/:id', function(req, res, next) {
-  res.send('respond with a resource');
+  couch.get("bizlist", req.params.id).then(({data, headers, status}) => {
+    couch.del("bizlist", req.params.id, data._rev).then(({data, headers, status}) => {
+      req.flash('success', 'Business Removed');
+      res.redirect('/businesses');
+  }, err => {
+      res.send(err);
+  });
+  }, err => {
+      res.send(err);
+  });
 });
 
 module.exports = router;
